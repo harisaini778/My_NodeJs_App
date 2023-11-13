07cfae05ec61f701535f67f3308ca8e5e2d9dc87
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-11-13 09:16:24 +0530

    understanding to redirecting the request and fs

--- app.js
+++ app.js
@@ -13,9 +13,27 @@
 // server.listen(3000);
 
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer(function rqListener(req, res) {
     const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Submit</button></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     
     if (url === '/home') {
         res.setHeader('Content-Type', 'text/html');
