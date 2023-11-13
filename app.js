17fa0bed6513b572ca3eca489eefb569184ba81d
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-11-13 08:11:57 +0530

    added a code to send res to the server

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
-const server = http.createServer(
-    function rqListener(req,res) {
-    console.log(req);
-}
-);
+const server = http.createServer(function rqListener(req, res) {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body>Hello From Server!</body>');
+    res.write('</html>');
+    res.end();
+});
+
 server.listen(3000);
